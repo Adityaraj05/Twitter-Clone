b5dd0ae1348eaa6caab15d50755d13d9e6f9921a
Aditya raj <adityarajavi05@gmail.com>
2023-05-28 23:45:57 +0530

    created sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import SidebarMenuItem from "./SidebarMenuItem";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  BellIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+  HashtagIcon,
+  InboxIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+export default function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+      {/* Twitter Logo */}
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
+        <Image
+          width="50"
+          height="50"
+          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+        ></Image>
+      </div>
+
+      {/* Menu */}
+
+      <div className="mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active/>
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+
+      {/* Button */}
+
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
+
+      {/* Mini-Profile */}
+
+      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+        <img
+          src="https://yt3.ggpht.com/M9SMtoHULi1LF3DvkJ6PeVInfEwWyhjBU6Drf-Z9kmqRc6SWoYfY4rOhe2n9SQ26qPTn3u_odqI=s88-c-k-c0x00ffffff-no-rj-mo"
+          alt="user-img"
+          className="h-10 w-10 rounded-full xl:mr-2"
+        />
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold">Aditya Raj</h4>
+          <p className="text-gray-500">@Adityarjavi05</p>
+        </div>
+        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline"/>
+      </div>
+    </div>
+  );
+}
