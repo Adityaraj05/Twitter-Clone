d052b4ac92295e8f05aa3605a32a309bf043c29f
Aditya raj <adityarajavi05@gmail.com>
2023-05-29 14:44:30 +0530

    send data to firebase and add loading effect

--- components/Widgets.js
+++ components/Widgets.js
@@ -11,31 +11,31 @@ export default function Widgets({ newsResults,  randomUsersResults  }) {
         <div className="flex items-center p-3 rounded-full relative">
           <SearchIcon className="h-5 z-50 text-gray-500" />
           <input
-            className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100 "
+            className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-200 "
             type="text"
             placeholder="Search Twitter"
           />
         </div>
       </div>
 
-      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+      <div className="text-gray-700 space-y-3 bg-gray-300 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">Whats happening</h4>
         {newsResults.slice(0, articleNum).map((article) => (
           <News key={article.title} article={article} />
         ))}
         <button
           onClick={() => setArticleNum(articleNum + 3)}
-          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+          className="text-blue-400 pl-4 pb-3 hover:text-blue-500 "
         >
           Show more
         </button>
       </div>
-      <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%]">
+      <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-300 pt-2 rounded-xl w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">Who to follow</h4>
         {randomUsersResults?.slice(0, randomUserNum).map((randomUser) => (
           <div
             key={randomUser.login.username}
-            className="flex items-center px-4 py-2  cursor-pointer hover:bg-gray-200"
+            className="flex items-center px-4 py-2  cursor-pointer hover:bg-gray-300"
           >
             <img
               className="rounded-full"
@@ -58,7 +58,7 @@ export default function Widgets({ newsResults,  randomUsersResults  }) {
         ))}
         <button
           onClick={() => setRandomUserNum(randomUserNum + 3)}
-          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+          className="text-blue-400 pl-4 pb-3 hover:text-blue-500"
         >
         
           Show more
