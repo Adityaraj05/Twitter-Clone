5b70db083a5b3453544e2f2041b4881055977db5
Aditya raj <adityarajavi05@gmail.com>
2023-05-30 11:28:27 +0530

    send comment to firestore

--- components/CommentModal.js
+++ components/CommentModal.js
@@ -1,6 +1,7 @@
 import { useRecoilState } from "recoil";
 
 import { modalState, postIdState } from "../atom/modalAtom";
+import { useRouter } from "next/router";
 import Modal from "react-modal";
 import {
   EmojiHappyIcon,
@@ -9,7 +10,14 @@ import {
 } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
-import { doc, onSnapshot } from "firebase/firestore";
+import {
+    addDoc,
+    collection,
+    doc,
+    onSnapshot,
+    serverTimestamp,
+  } from "firebase/firestore";
+
 import Moment from "react-moment";
 import { useSession } from "next-auth/react";
 export default function CommentModal() {
@@ -18,6 +26,7 @@ export default function CommentModal() {
   const [post, setPost] = useState({});
   const [input, setInput] = useState("");
   const { data: session } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
     onSnapshot(doc(db, "posts", postId), (snapshot) => {
@@ -25,7 +34,20 @@ export default function CommentModal() {
     });
   }, [postId, db]);
 
-  function sendComment() {}
+  async function sendComment() {
+    await addDoc(collection(db, "posts", postId, "comments"), {
+      comment: input,
+      name: session.user.name,
+      username: session.user.username,
+      userImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    setOpen(false);
+    setInput("");
+    router.push(`posts/${postId}`);
+  }
+
   return (
     <div>
     {open && (
